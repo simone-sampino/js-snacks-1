933d71000d70506bb92517ea1bde869d3e6f2f53
simone-sampino <s.samp98@gmail.com>
2025-05-30 12:34:32 +0200

    Add HTML and JavaScript files for Snack 4

---
+++ snack-4/js/main-.js
@@ -0,0 +1,14 @@
+/* crea un array vuoto. chiedi per 6 volte di inserire un numero. se è dispari, inseriscilo nell'array */
+
+let number = [];
+let repeat = 6;
+
+for (let i = 0; i < repeat; i++) {
+    numbers = Number(prompt('Inserisci un numero'));
+    
+    if(numbers % 2) {
+        console.log(numbers);
+    } else {
+        console.log();
+    }
+}
